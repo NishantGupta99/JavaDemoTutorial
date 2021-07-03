dfbb68eeacd054a5b07999bf47d2ac6cac2fda70
nishant Gupta <ngactive99@gmail.com>
2021-07-03 17:45:29 +0530

    added age

--- src/com/company/DemoArrayList.java
+++ src/com/company/DemoArrayList.java
@@ -4,6 +4,8 @@ import java.util.*;
 public class DemoArrayList {
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<String>(Arrays.asList("Nishant ", "Ram", "sam"));
+        ArrayList<Integer> age = new ArrayList<Integer>(Arrays.asList(22,45,33,23,54));
+        System.out.println(age);
 
         System.out.println(names);
 
