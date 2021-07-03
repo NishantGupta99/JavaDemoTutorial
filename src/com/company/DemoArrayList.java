40c207bf784568e54cd701a6382d1296121c2de8
nishant Gupta <ngactive99@gmail.com>
2021-07-03 17:57:16 +0530

    used list.of to create a list

--- src/com/company/DemoArrayList.java
+++ src/com/company/DemoArrayList.java
@@ -3,12 +3,15 @@ import java.util.*;
 
 public class DemoArrayList {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>(Arrays.asList("Nishant ", "Ram", "sam"));
-        ArrayList<Integer> age = new ArrayList<Integer>(Arrays.asList(22,45,33,23,54));
+        ArrayList<String> names = new ArrayList<String>(Arrays.asList("Nishant ", "Ram", "sam")); // array list with string created by a single line
+        ArrayList<Integer> age = new ArrayList<Integer>(Arrays.asList(22,45,33,23,54)); // array list for integer created using new array list
+        List<String> name = List.of("patrick", "steward"); // array list created using a List.of method
+        List<Integer> ages =List.of(44,55,34,56,33,11,33,54,23,6,76,23); // disadvantage of this is no value can be added afterwords
+
         System.out.println(age);
-
+        System.out.println(ages);
         System.out.println(names);
-
+        System.out.println(name);
 
     }
 }
