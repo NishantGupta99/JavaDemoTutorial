e072f94a39af32868b0bf3790287c5b7728a7dea
nishant Gupta <ngactive99@gmail.com>
2021-07-04 13:16:36 +0530

    Learning to add, remove adn edit elements of Hashsets

--- src/com/company/HashSet.java
+++ src/com/company/HashSet.java
@@ -13,5 +13,22 @@ public class HashSet {
         //System.out.println(DataBase); // will print the entire database
         //System.out.println((DataBase.size())); // will print the size of the data base
 
+        // changing variable in hashmap
+
+        HashMap<Integer , String> Menu = new HashMap<>();
+
+        Menu.put(1,"Chicken");
+        Menu.put(2,"soup");
+        Menu.put(3,"pasta");
+        System.out.println(Menu);
+
+        //Menu.put(2,"Steak"); // enter the value using the same serial number for the reference
+
+        //System.out.println(Menu);
+
+        Menu.remove(3);
+
+        System.out.println(Menu);
+
     }
 }
