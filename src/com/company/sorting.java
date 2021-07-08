8ff89ea9cfe54e3835ff275e2c8027204130584a
nishant Gupta <ngactive99@gmail.com>
2021-07-08 19:45:57 +0530

    Sorted a group of integers in decending orders

--- src/com/company/sorting.java
+++ src/com/company/sorting.java
@@ -1,5 +1,6 @@
 package com.company;
 import java.util.Arrays;
+import java.util.Collections; // to use the decending order
 
 public class sorting {
     public static void main(String[] args) {
@@ -7,11 +8,17 @@ public class sorting {
         // in this file we will learn about soring of arrays
         Integer[] numbers = new Integer[] { 15, 11, 9, 55, 47, 18, 520, 1123, 366, 420 }; // the array that we will be sorting
 
-        Arrays.sort(numbers); // the command to sort the following code
+       // Arrays.sort(numbers); // the command to sort the following code
+
+
+
+        // now lets try to sort the array into decending order
+
+
+        Arrays.sort(numbers, Collections.reverseOrder()); // we are revesing the collections
+
 
         System.out.println(Arrays.toString(numbers)); // to print the array in the form of a string
-
-        //
 
 
 
