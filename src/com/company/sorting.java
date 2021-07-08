27fbcbea5af94be735b02f35c878b3cf934c3edd
nishant Gupta <ngactive99@gmail.com>
2021-07-08 19:35:54 +0530

    Sorted a group of integers in assending orders

--- src/com/company/sorting.java
+++ src/com/company/sorting.java
@@ -1,10 +1,17 @@
 package com.company;
+import java.util.Arrays;
 
 public class sorting {
     public static void main(String[] args) {
 
         // in this file we will learn about soring of arrays
         Integer[] numbers = new Integer[] { 15, 11, 9, 55, 47, 18, 520, 1123, 366, 420 }; // the array that we will be sorting
+
+        Arrays.sort(numbers); // the command to sort the following code
+
+        System.out.println(Arrays.toString(numbers)); // to print the array in the form of a string
+
+        //
 
 
 
