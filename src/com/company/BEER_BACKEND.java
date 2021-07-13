5709636444522a76a076f6e380b7aeadc475053d
nishant Gupta <ngactive99@gmail.com>
2021-07-14 01:33:22 +0530

    looping , conditional opertator in one program

---
+++ src/com/company/BEER_BACKEND.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class BEER_BACKEND {
+    public static void main(String[] args) {
+        int beerNum = 99;
+        String word = "bottles";
+
+        while(beerNum>0) {
+            if(beerNum==1){
+                word = "bottle"; // to make it singular
+            }
+            System.out.println(beerNum + " " + word + "of beer on the wall" );
+            System.out.println(beerNum + " " + word + " of beer");
+            System.out.println("take one down");
+            System.out.println("pass it around");
+            beerNum = beerNum - 1;
+            if(beerNum>0){
+                System.out.println(beerNum +" " + word + "of beer on the wall");
+            }
+            else {
+                System.out.println("we are out of beer");
+            }
+
+        }
+
+
+    }
+}
